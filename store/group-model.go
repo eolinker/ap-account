ae4d5f0e1fd2ac1229b6edd09efc3b60e8de48d5
hmzzrcs <hmzzrcs@163.com>
2024-03-08 15:28:15 +0800

    修正数据库结构

--- store/group-model.go
+++ store/group-model.go
@@ -4,8 +4,8 @@ import "time"
 
 type UserGroup struct {
 	Id         int64     `gorm:"type:BIGINT(20);size:20;not null;auto_increment;primary_key;column:id;comment:主键ID;"`
-	UUID       string    `gorm:"size:36;not null;column:uuid;uniqueIndex:gid;comment:分组id;uniqueIndex:gid_uid;"`
-	Name       string    `gorm:"size:36;not null;column:name;uniqueIndex:name;comment:分组名称;uniqueIndex:gid_uid;"`
+	UUID       string    `gorm:"size:36;not null;column:uuid;uniqueIndex:gid;comment:分组id;"`
+	Name       string    `gorm:"size:36;not null;column:name;uniqueIndex:name;comment:分组名称;"`
 	Creator    string    `gorm:"size:36;not null;column:creator;index:creator;comment:创建人;"`
 	CreateTime time.Time `gorm:"type:timestamp;NOT NULL;DEFAULT:CURRENT_TIMESTAMP;column:create_at;comment:创建时间"`
 }
