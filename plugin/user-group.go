70344e5d138a4769f8680e5aa512fd33e81cf57e
Liujian <824010343@qq.com>
2024-04-17 17:56:00 +0800

    修改用户分组编辑接口

--- plugin/user-group.go
+++ plugin/user-group.go
@@ -13,7 +13,7 @@ func (p *plugin) getUserGroupAPIs() []pm3.Api {
 		pm3.CreateApiWidthDoc(http.MethodGet, "/api/v1/user/group/:id", []string{"context", "path:id"}, []string{"user_group"}, p.userGroupController.Get),
 		pm3.CreateApiWidthDoc(http.MethodPost, "/api/v1/user/group", []string{"context", "query:id", "body"}, []string{}, p.userGroupController.Create),
 		pm3.CreateApiWidthDoc(http.MethodPut, "/api/v1/user/group/:id", []string{"context", "path:id", "body"}, []string{}, p.userGroupController.Save),
-		pm3.CreateApiWidthDoc(http.MethodPut, "/api/v1/user/group", []string{"context", "query:id", "body"}, []string{}, p.userGroupController.Save),
+		pm3.CreateApiWidthDoc(http.MethodPut, "/api/v1/user/group", []string{"context", "query:user_group", "body"}, []string{}, p.userGroupController.Save),
 		pm3.CreateApiWidthDoc(http.MethodDelete, "/api/v1/user/group/:id", []string{"context", "path:id"}, []string{}, p.userGroupController.Delete),
 		pm3.CreateApiWidthDoc(http.MethodDelete, "/api/v1/user/group", []string{"context", "query:id"}, []string{}, p.userGroupController.Delete),
 		pm3.CreateApiWidthDoc(http.MethodGet, "/api/v1/simple/user-groups", []string{"context"}, []string{"user-groups"}, p.userGroupController.Simple),
